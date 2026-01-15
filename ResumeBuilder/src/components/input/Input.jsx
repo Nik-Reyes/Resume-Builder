a4350623cdd47f0ab9c51c445d0b83394b143e3a
“Nik <nikolaspreyes@gmail.com>
2026-01-14 16:10:04 -0800

    style: add conditional class and styles

--- ResumeBuilder/src/components/input/Input.jsx
+++ ResumeBuilder/src/components/input/Input.jsx
@@ -1,6 +1,16 @@
 function InputField({ title, inputType }) {
+  function getInputClass(title) {
+    switch (title) {
+      case "Start Date":
+        return "input-control start-date";
+      case "End Date":
+        return "input-control end-date";
+      default:
+        return "input-control";
+    }
+  }
   return (
-    <div className="input-control">
+    <div className={getInputClass(title)}>
       <label htmlFor="form-input">
         <span className="form-input-title">{title}</span>{" "}
         <input id="form-input" type={inputType} placeholder="" />
