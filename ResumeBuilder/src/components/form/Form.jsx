432e955ebc2de902bfded7f6adc48bd1fe30fea8
“Nik <nikolaspreyes@gmail.com>
2026-01-22 18:02:53 -0800

    style: add box shadows and sticky form header

--- ResumeBuilder/src/components/form/Form.jsx
+++ ResumeBuilder/src/components/form/Form.jsx
@@ -3,13 +3,13 @@ function Form({ title, replicable, addInputGroup, children }) {
     <div className="form">
       <div className="form-title-wrapper">
         <span className="form-title">{title}</span>
+        {replicable && (
+          <button data-button-type="add-inputGroup" onClick={addInputGroup}>
+            + add {title.toLowerCase()}
+          </button>
+        )}
       </div>
       <div className="input-groups">{children}</div>
-      {replicable && (
-        <button data-button-type="add-inputGroup" onClick={addInputGroup}>
-          + add {title.toLowerCase()}
-        </button>
-      )}
     </div>
   );
 }
