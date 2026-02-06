dc6a9d5ccb9dc2e21eb499f82f15d4f003d5b342
“Nik <nikolaspreyes@gmail.com>
2026-02-05 18:06:38 -0800

    refactor: add logic to exlude any empty section from being rendered in resume

--- ResumeBuilder/src/components/resume/Resume.jsx
+++ ResumeBuilder/src/components/resume/Resume.jsx
@@ -1,22 +1,28 @@
-import { formState } from "../../data/form-state.js";
 import { sectionDirectory } from "../../helpers/component-directory.jsx";
 
-function Resume() {
+function Resume({ formData }) {
   return (
     <div className="resume-wrapper">
       <div className="resume-page">
-        {Object.keys(formState).map((sectionName) => {
-          const lowercaseSection = sectionName.toLowerCase();
-          const sectionComponent = sectionDirectory[lowercaseSection];
-          if (!sectionComponent) return null;
+        {Object.keys(formData).map((sectionName) => {
+          const sectionValues = formData[sectionName]
+            .flatMap((obj) => Object.values(obj))
+            .filter((item) => typeof item !== "number");
+          const isEmptySection = sectionValues.every((val) => val === "");
+          const sectionComponent = sectionDirectory(sectionName, formData);
+          if (!sectionComponent || isEmptySection) return null;
 
           return (
             <div className={`section ${sectionName}`} key={sectionName}>
-              <div className="section-title">{sectionName}</div>
-              <div className="bar"></div>
-              <div className="section-content">
-                {sectionDirectory[lowercaseSection](sectionName)}
-              </div>
+              {sectionName.includes("personal") ? (
+                sectionComponent
+              ) : (
+                <>
+                  <div className="section-title">{sectionName}</div>
+                  <div className="bar"></div>
+                  <div className="section-content">{sectionComponent}</div>
+                </>
+              )}
             </div>
           );
         })}
