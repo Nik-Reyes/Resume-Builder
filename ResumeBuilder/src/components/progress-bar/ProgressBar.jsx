28079dfe917307ce44397cd9b378100d558d37d4
“Nik <nikolaspreyes@gmail.com>
2026-02-05 18:09:25 -0800

    refactor: add classname to progress tag

--- ResumeBuilder/src/components/progress-bar/ProgressBar.jsx
+++ ResumeBuilder/src/components/progress-bar/ProgressBar.jsx
@@ -1,5 +1,4 @@
 function ProgressBar({ progPercent }) {
-  console.log(progPercent);
   const hundredthAdjustedProg = progPercent * 100;
   return (
     <div className="progress-bar-wrapper">
@@ -7,7 +6,11 @@ function ProgressBar({ progPercent }) {
         <div className="progress-title">Progress</div>
         <div className="progress-percent">{hundredthAdjustedProg}%</div>
       </div>
-      <progress max={100} value={hundredthAdjustedProg}></progress>
+      <progress
+        max={100}
+        value={hundredthAdjustedProg}
+        className="progressbar"
+      ></progress>
     </div>
   );
 }
