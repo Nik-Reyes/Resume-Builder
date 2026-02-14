c1566e226ea8a93e6d4286fe7ad29ceec604d948
“Nik <nikolaspreyes@gmail.com>
2026-02-14 02:40:21 -0800

    refactor(fix): update onChange to properly update state

--- ResumeBuilder/src/components/form/SkillGroup.jsx
+++ ResumeBuilder/src/components/form/SkillGroup.jsx
@@ -13,50 +13,58 @@ function SkillGroup({
   const { name, title, type } = currentFormInputGroup.input;
   const skills = groupStateObj.skillItems;
 
-  function onInputChange(e) {
+  function onInputChange(e, skillToUpdate) {
     const newVal = e.target.value;
-
-    console.log(newVal);
-    // const updatedGroup = {
-    //   ...groupStateObj,
-    //   [input.name]: newVal,
-    // };
-    // handleInputChange(updatedGroup);
+    const updatedGroup = {
+      ...groupStateObj,
+      skillItems: skills.map((skill) =>
+        skill.id === skillToUpdate.id ? { ...skill, skill: newVal } : skill
+      ),
+    };
+    handleInputChange(updatedGroup);
   }
 
   return (
-    <div className="skill-wrapper">
+    <div className="skill-category-wrapper">
       <div className="skill-type">
         <input
           placeholder={groupStateObj.category || "i.e., Languages"}
         ></input>
       </div>
-      <div className="skill-group">
-        <div className="replicable accordian">
-          <div className="accordian-panel">
-            <GroupHeading
-              titleData={{
-                title: groupStateObj.skill,
-                titlePlaceholder: "Skill",
-              }}
-              deleteGroup={handleDeleteGroup}
-              toggleAccordian={() =>
-                handleToggleGroup(currentFormSection, groupStateObj.id)
-              }
-              hidden={hidden}
-            />
-            <div className="accordian-content" aria-hidden={hidden}>
-              <div>
-                <InputField
-                  title={title}
-                  inputType={type}
-                  inputValue={"stuff"}
-                  onChange={(e) => onInputChange(e)}
+      {/* for each skill-category-wrapper there are n replicable accordian */}
+      <div className="skills-wrapper">
+        {skills.map((skillObj) => {
+          return (
+            <div
+              className="replicable accordian"
+              key={`skill-item=${skillObj.id}`}
+            >
+              <div className="accordian-panel">
+                <GroupHeading
+                  titleData={{
+                    title: skillObj[name],
+                    titlePlaceholder: "Skill",
+                  }}
+                  deleteGroup={handleDeleteGroup}
+                  toggleAccordian={() =>
+                    handleToggleGroup(currentFormSection, groupStateObj.id)
+                  }
+                  hidden={hidden}
                 />
+                <div className="accordian-content" aria-hidden={hidden}>
+                  <div>
+                    <InputField
+                      title={title}
+                      inputType={type}
+                      inputValue={skillObj[name]}
+                      onChange={(e) => onInputChange(e, skillObj)}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
