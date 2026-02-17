d02fcec12e8a24fb2c576af879d0c992f7c17348
“Nik <nikolaspreyes@gmail.com>
2026-02-17 15:37:01 -0800

    refactor: remove progress label and percent marker, leave only the bar

--- ResumeBuilder/src/components/progress-footer/ProgressBar.jsx
+++ ResumeBuilder/src/components/progress-footer/ProgressBar.jsx
@@ -2,10 +2,6 @@ function ProgressBar({ progPercent }) {
   const hundredthAdjustedProg = parseInt(progPercent * 100);
   return (
     <div className="progress-bar-wrapper">
-      <div className="progress-header">
-        <div className="progress-title">Progress</div>
-        <div className="progress-percent">{hundredthAdjustedProg}%</div>
-      </div>
       <progress
         max={100}
         value={hundredthAdjustedProg}
