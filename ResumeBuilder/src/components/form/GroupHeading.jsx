a93362b5365bdeb9bf71c62b20b32bdfa782b3b1
“Nik <nikolaspreyes@gmail.com>
2026-02-17 16:28:46 -0800

    refactor: move svgs to asset file, import using svgr

--- ResumeBuilder/src/components/form/GroupHeading.jsx
+++ ResumeBuilder/src/components/form/GroupHeading.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Meatball from "../../assets/svg/meatball.svg?react";
+import VChevron from "../../assets/svg/verticalChevron.svg?react";
 
 function GroupHeading({
   titleData,
@@ -6,6 +8,7 @@ function GroupHeading({
   deleteActiveGroup,
   toggleAccordian,
   hidden,
+  children,
 }) {
   const [menuClass, setMenuClass] = useState("");
   // start with no class, no styles applied
@@ -18,17 +21,21 @@ function GroupHeading({
   return (
     <div className="group-header">
       <header className="group-title-wrapper">
-        <h1 className="group-title">
-          {titleData.title === ""
-            ? titleData.titlePlaceholder
-            : titleData.title}
-        </h1>
-        {"subTitle" in titleData && (
-          <p className="group-subtitle">
-            {titleData.subTitle === ""
-              ? titleData.subTitlePlaceholder
-              : titleData.subTitle}
-          </p>
+        {children || (
+          <>
+            <h1 className="group-title">
+              {titleData.title === ""
+                ? titleData.titlePlaceholder
+                : titleData.title}
+            </h1>
+            {"subTitle" in titleData && (
+              <p className="group-subtitle">
+                {titleData.subTitle === ""
+                  ? titleData.subTitlePlaceholder
+                  : titleData.subTitle}
+              </p>
+            )}
+          </>
         )}
       </header>
       <div className="edit-group-btn-wrapper">
@@ -42,37 +49,16 @@ function GroupHeading({
             className={menuClass}
             disabled={menuClass !== "open"}
           >
-            Delete
+            <span>Delete</span>
           </button>
           <button data-button-type="edit-group" onClick={toggleMenu}>
-            <svg
-              width="16px"
-              height="16px"
-              viewBox="0 0 16 16"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-            >
-              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-            </svg>
+            <Meatball />
           </button>
           <button
             data-button-type="accordian-control"
             onClick={toggleAccordian}
           >
-            <svg
-              width="24px"
-              height="24px"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className={hidden ? "hidden" : "visible"}
-            >
-              <polyline points="18 15 12 9 6 15"></polyline>
-            </svg>
+            <VChevron className={hidden ? "hidden" : "visible"} />
           </button>
         </div>
       </div>
