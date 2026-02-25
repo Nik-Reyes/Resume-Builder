8dadaba8f6f3017bcb717160e668699073d7d108
“Nik <nikolaspreyes@gmail.com>
2026-02-25 12:55:19 -0800

    refactor: extract all logic from footer into parent component

--- ResumeBuilder/src/components/footer/Footer.jsx
+++ ResumeBuilder/src/components/footer/Footer.jsx
@@ -1,53 +1,8 @@
-import ProgressBar from "./ProgressBar.jsx";
-
-function Footer({ formNumber, totalForms, setFormNumber, manageView }) {
-  const isLastForm = formNumber === totalForms;
-  const prog =
-    formNumber === 0
-      ? 1 / (totalForms + 1)
-      : (formNumber + 1) / (totalForms + 1);
-
-  // ProgressFooter Handlers
-  function incrementFormNumber() {
-    if (formNumber >= totalForms) return;
-    setFormNumber(formNumber + 1);
-  }
-
-  function decrementFormNumber() {
-    if (formNumber <= 0) return;
-    setFormNumber(formNumber - 1);
-  }
-
+function Footer({ className = "", children }) {
   return (
-    <div className="progress-footer container">
+    <div className={className}>
       <div className="spacer"></div>
-      <div className="progress-content">
-        <ProgressBar progPercent={prog} />
-        <div className="progress-buttons-wrapper">
-          <button
-            className="progress-button sharp-white"
-            onClick={() => decrementFormNumber()}
-            disabled={formNumber >= 1 ? false : true}
-          >
-            <span>Previous</span>
-          </button>
-          <button
-            className={
-              isLastForm
-                ? "progress-button disabled"
-                : "progress-button sharp-white"
-            }
-            onClick={() => {
-              if (isLastForm) {
-                manageView("resume");
-              }
-              incrementFormNumber();
-            }}
-          >
-            <span>{isLastForm ? "Finish" : "Next"}</span>
-          </button>
-        </div>
-      </div>
+      <div className="footer-content">{children}</div>
     </div>
   );
 }
