fa65a91fc1f58b8c4e1029f5eec14ac963415c66
“Nik <nikolaspreyes@gmail.com>
2026-02-25 12:55:54 -0800

    refactor: compose footer with button wrapper and button components

--- ResumeBuilder/src/Views/Mobile.jsx
+++ ResumeBuilder/src/Views/Mobile.jsx
@@ -1,11 +1,12 @@
-import { forms } from "../data/form-config.js";
 import Form from "../components/form/Form.jsx";
 import Resume from "../components/resume/Resume.jsx";
 import FormEntries from "../components/form/FormEntry.jsx";
 import Footer from "../components/footer/Footer.jsx";
+import ProgressBar from "../components/footer/ProgressBar.jsx";
+import ButtonWrapper from "../components/button/ButtonWrapper.jsx";
+import Button from "../components/button/button.jsx";
 
 function Mobile({ view, manageView, currentFormConfig, mobileProps }) {
-  const totalForms = Object.keys(forms).length - 1;
   const staticSharedProps = {
     currentFormInputFields: mobileProps.currentFormInputFields,
     toggleAccordion: mobileProps.toggleAccordion,
@@ -20,32 +21,60 @@ function Mobile({ view, manageView, currentFormConfig, mobileProps }) {
     <div className="content">
       <div className="main">
         {view.form ? (
-          <Form
-            formName={currentFormConfig.formName}
-            formIsReplicable={mobileProps.formIsReplicable}
-            formButtonName={currentFormConfig.formButtonName}
-            onClick={mobileProps.handleAddGroup}
-          >
-            <FormEntries
-              currentFormData={mobileProps.currentFormData}
-              staticSharedProps={staticSharedProps}
-              titleDataMap={mobileProps.titleDataMap}
-              customRender={mobileProps.currentFormConfig.customRender}
-              currentFormSection={mobileProps.currentFormSection}
-            />
-          </Form>
+          <>
+            <Form
+              formName={currentFormConfig.formName}
+              formIsReplicable={mobileProps.formIsReplicable}
+              formButtonName={currentFormConfig.formButtonName}
+              onClick={mobileProps.handleAddGroup}
+            >
+              <FormEntries
+                currentFormData={mobileProps.currentFormData}
+                staticSharedProps={staticSharedProps}
+                titleDataMap={mobileProps.titleDataMap}
+                customRender={mobileProps.currentFormConfig.customRender}
+                currentFormSection={mobileProps.currentFormSection}
+              />
+            </Form>
+            <Footer className="progress-footer container">
+              <ProgressBar progPercent={mobileProps.prog} />
+              <ButtonWrapper className="progress-button-wrapper">
+                <Button
+                  className="progress-button sharp-white"
+                  onClick={mobileProps.decrementFormNumber}
+                  disabled={mobileProps.formNumber <= 0}
+                >
+                  <span>Previous</span>
+                </Button>
+                <Button
+                  className={
+                    mobileProps.isLastForm
+                      ? "progress-button disabled"
+                      : "progress-button sharp-white"
+                  }
+                  onClick={() => {
+                    if (mobileProps.isLastForm) {
+                      manageView("resume");
+                    }
+                    mobileProps.incrementFormNumber();
+                  }}
+                >
+                  <span>{mobileProps.isLastForm ? "Finish" : "Next"}</span>
+                </Button>
+              </ButtonWrapper>
+            </Footer>
+          </>
         ) : (
-          <Resume formData={mobileProps.formData} />
+          <>
+            <Resume formData={mobileProps.formData} />
+            <Footer className="resume-footer container">
+              <ButtonWrapper className="button-wrapper container">
+                <Button className="download-resume-button">Download</Button>
+              </ButtonWrapper>
+            </Footer>
+          </>
         )}
       </div>
-      {view.form && (
-        <Footer
-          formNumber={mobileProps.formNumber}
-          totalForms={totalForms}
-          setFormNumber={mobileProps.setFormNumber}
-          manageView={manageView}
-        />
-      )}
     </div>
   );
 }
