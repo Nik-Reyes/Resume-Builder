b795643d27bbe578557ca5e6d0b32222e9dd5635
“Nik <nikolaspreyes@gmail.com>
2026-03-05 16:40:22 -0800

    refactor: compose header

--- ResumeBuilder/src/App.jsx
+++ ResumeBuilder/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 import Header from "./components/Header/Header.jsx";
 import Content from "./components/content/Content.jsx";
+import Button from "./components/button/Button.jsx";
 
 function App() {
-  const [view, setView] = useState({ form: true, resume: false });
+  const [view, setView] = useState({
+    form: true,
+    resume: false,
+    customize: false,
+  });
+  const isMobile = useMediaQuery({ maxWidth: 900 });
 
   function manageView(newView) {
     const updateView = { ...view };
@@ -13,10 +20,50 @@ function App() {
     setView(updateView);
   }
 
+  useEffect(() => {
+    if (!isMobile && view.resume) {
+      manageView("customize");
+    }
+  }, [isMobile]);
+
   return (
     <div className="page-wrapper">
-      <Header view={view} manageView={manageView} />
-      <Content view={view} manageView={manageView} />
+      <Header>
+        <Button
+          className={
+            view.form
+              ? "form-view sharp-white"
+              : "form-view sharp-white inactive"
+          }
+          onClick={() => manageView("form")}
+        >
+          <span>Edit Form</span>
+        </Button>
+        {isMobile ? (
+          <Button
+            className={
+              view.form
+                ? "resume-view sharp-white inactive"
+                : "resume-view sharp-white"
+            }
+            onClick={() => manageView("resume")}
+          >
+            <span>Preview Resume</span>
+          </Button>
+        ) : (
+          <Button
+            className={
+              view.form
+                ? "resume-view sharp-white inactive"
+                : "resume-view sharp-white"
+            }
+            onClick={() => manageView("customize")}
+          >
+            <span>Customize</span>
+          </Button>
+        )}
+      </Header>
+      <Content view={view} manageView={manageView} isMobile={isMobile} />
     </div>
   );
 }
--- ResumeBuilder/src/components/header/Header.jsx
+++ ResumeBuilder/src/components/header/Header.jsx
@@ -1,26 +1,5 @@
-function Header({ view, manageView }) {
-  return (
-    <div className="view-header container">
-      <button
-        className={
-          view.form ? "form-view sharp-white" : "form-view sharp-white inactive"
-        }
-        onClick={() => manageView("form")}
-      >
-        <span>Edit Form</span>
-      </button>
-      <button
-        className={
-          view.form
-            ? "resume-view sharp-white inactive"
-            : "resume-view sharp-white"
-        }
-        onClick={() => manageView("resume")}
-      >
-        <span>Preview Resume</span>
-      </button>
-    </div>
-  );
+function Header({ children }) {
+  return <div className="view-header container">{children}</div>;
 }
 
 export default Header;
